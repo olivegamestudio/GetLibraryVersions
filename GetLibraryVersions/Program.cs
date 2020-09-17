f4e432e34388e5ae03b1f122d785b1602566769e
Simon Jefferies <simon.jefferies@ingg.com>
2020-09-17 18:22:49 +0100

    Imported.

---
+++ GetLibraryVersions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace GetLibraryVersions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|Library|Version|");
+            Console.WriteLine("|-|-|");
+
+            foreach (string fileName in Directory.GetFiles(args[0]))
+            {
+                if (fileName.EndsWith(".dll") || fileName.EndsWith(".exe"))
+                {
+                    FileVersionInfo f = FileVersionInfo.GetVersionInfo(fileName);
+                    if (!string.IsNullOrEmpty(f.InternalName))
+                    {
+                        Console.WriteLine($"|{f.InternalName}|{f.FileVersion}|");
+                    }
+                }
+            }
+        }
+    }
+}
